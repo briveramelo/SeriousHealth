c3508957b6595867e654849ac57733aaacb1791d
BRANDON RIVERA-MELO <u1089885@ad.utah.edu>
2016-10-06 23:36:40 -0600

    added simple phoneme bank and speech evaluator singleton classes and put them in a mainmenu scene

---
+++ Assets/Scripts/PhonemeBank.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public enum Phoneme {
+    La=0,
+    Di=1,
+    Da=2
+}
+
+public class PhonemeBank : MonoBehaviour {
+
+    //public reference for other classes to access
+    public static PhonemeBank Instance;
+
+    /// <summary>
+    /// Collection of phonemes and their unlock status
+    /// </summary>
+    static Dictionary<Phoneme, bool> phonemeIsUnlocked = new Dictionary<Phoneme, bool>() {
+        {Phoneme.La, true },
+        {Phoneme.Di, true },
+        {Phoneme.Da, true }
+    };
+
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Tells you if the phoneme in question is already unlocked
+    /// </summary>
+    public static bool IsPhonemeUnlocked(Phoneme phonemeInQuestion) {
+        return phonemeIsUnlocked[phonemeInQuestion];
+    }
+
+    /// <summary>
+    /// Checks if you should unlock a new phoneme, and if so, unlocks that phoneme
+    /// </summary>
+    public static void CheckToUnlockNewPhoneme() {
+        if (true) {
+            UnlockNewPhoneme(0);
+        }
+    }
+
+
+    /// <summary>
+    /// Unlocks the requested phoneme
+    /// </summary>
+    static void UnlockNewPhoneme(Phoneme phonemeToUnlock) {
+        phonemeIsUnlocked[phonemeToUnlock] = true;
+    }
+}
---
+++ Assets/Scripts/SpeechEvaluator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Evaluation {
+    Bad=0,
+    OK=1,
+    Great=2
+}
+
+public class SpeechEvaluator : MonoBehaviour {
+
+    //public reference for other classes to access
+    public static SpeechEvaluator Instance;
+   
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+}
